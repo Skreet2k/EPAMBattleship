97f5fd52382f27cee9b3e39b52150f815f5a181e
Skreet2k <Skreet2k@gmail.com>
2016-09-02 02:09:23 +0400

    Add files via upload

---
+++ BattleShip/Server/GameService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "GameService" в коде и файле конфигурации.
+    [ServiceBehavior(
+        ConcurrencyMode = ConcurrencyMode.Single,
+        InstanceContextMode = InstanceContextMode.PerCall)]
+    public class GameService : IGameService
+    {
+        private static Dictionary<int, IGameServiceCallback> _callbackList = new Dictionary<int, IGameServiceCallback>();
+        private static int i;
+
+        public void CreateGame()
+        {
+            _callbackList[0].CallBack($"danuna? Zarabotalo? {i++}");
+        }
+
+        public void JoinGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JoinById(int id)
+        {
+            var callback = OperationContext.Current.GetCallbackChannel<IGameServiceCallback>();
+            _callbackList.Add(id, callback);
+        }
+
+        public void SendById(int id, string message)
+        {
+            if (_callbackList.ContainsKey(id))
+            {
+                _callbackList[id].CallBack(message);
+            }
+        }
+    }
+    
+}
---
+++ BattleShip/Server/IGameService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IGameService" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IGameServiceCallback))]
+    public interface IGameService
+    {
+        [OperationContract(IsOneWay = true)]
+        void CreateGame();
+        [OperationContract(IsOneWay = true)]
+        void JoinGame();
+        [OperationContract(IsOneWay = true)]
+        void JoinById(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendById(int id, string message);
+    }
+
+    [ServiceContract]
+    public interface IGameServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void CallBack(string message);
+    }
+}
